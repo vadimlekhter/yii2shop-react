503cf9eac6af8581b1940ada494aadb5e7672794
vadimlekhter <v_lehter@mail.ru>
2021-01-16 21:00:40 +0300

    AppSecond

--- src/AppSecond.js
+++ src/AppSecond.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function AppSecond() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Google
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default AppSecond;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppSecond from './AppSecond';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -9,6 +10,13 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <AppSecond />
+    </React.StrictMode>,
+    document.getElementById('root2')
 );
 
 // If you want to start measuring performance in your app, pass a function
